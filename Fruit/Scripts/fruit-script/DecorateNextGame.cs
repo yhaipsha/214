aa9fe0ae848aba9d6ce9ef29d7b45a46b7504835
alan moon <31900528@qq.com>
2013-02-21 23:43:18 +0800

    2-21
    home2

--- Fruit/Scripts/fruit-script/DecorateNextGame.cs
+++ Fruit/Scripts/fruit-script/DecorateNextGame.cs
@@ -4,35 +4,35 @@ using System.Collections;
 public class DecorateNextGame : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	public void Initialization ()
-	{
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	public Transform transGamePanel;
+    }
 
-	void OnClick ()
-	{
-		//next level
-		int nextLevel = PlayerPrefs.GetInt ("NowPlay")+1;
-		int currentMode = PlayerPrefs.GetInt ("NowMode") - 1;
-		string lastlevelName = Globe.jsonLableNames [currentMode] + (nextLevel-1);
-		print (nextLevel + ",lastLevelName = " + lastlevelName);
-		
-		PlayerPrefs.SetInt (lastlevelName, 3);
-		PlayerPrefs.SetInt ("NowPlay", nextLevel);
-		
-//		if (transGamePanel != null)
-//			transGamePanel.GetComponent<GamePlayLayer> ().initGameWindow (PlayerPrefs.GetInt ("NowPlay"));
-		
-		
-//		transGamePanel.GetComponent<TweenPosition>().Play(true);
-//		Globe.getPanelOfParent(transGamePanel,1,"Panel - GameWin").GetComponent<TweenPosition>().Reset();
-	}
+    public void Initialization()
+    {
+
+    }
+
+    public Transform transGamePanel;
+
+    void OnClick()
+    {
+        //next level
+        int currentLevel = PlayerPrefs.GetInt("NowPlay");
+        int currentMode = PlayerPrefs.GetInt("NowMode");
+        string lastlevelName = Globe.jsonLableNames[currentMode - 1] + currentLevel;
+        print(currentLevel + ",lastLevelName = " + lastlevelName);
+
+        PlayerPrefs.SetInt(lastlevelName, 3);
+        PlayerPrefs.SetInt("NowPlay", currentLevel + 1);
+        PlayerPrefs.Save();
+        //		if (transGamePanel != null)
+        //			transGamePanel.GetComponent<GamePlayLayer> ().initGameWindow (PlayerPrefs.GetInt ("NowPlay"));
+
+
+        //		transGamePanel.GetComponent<TweenPosition>().Play(true);
+        //		Globe.getPanelOfParent(transGamePanel,1,"Panel - GameWin").GetComponent<TweenPosition>().Reset();
+    }
 }
